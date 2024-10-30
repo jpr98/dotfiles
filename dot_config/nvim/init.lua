14a1a09bfe06bc5f90b3e13ab46b743d46f8040d
jpr98 <jprscs@gmail.com>
2024-10-30 12:42:40 -0600

    improvements to wezterm tabs and nvim config in general

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -322,14 +322,22 @@ require("lazy").setup({
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
 			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+			vim.keymap.set("n", "<leader>sf", function()
+				builtin.find_files({
+					hidden = true,
+				})
+			end, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>s.", function()
+				builtin.oldfiles({
+					only_cwd = true,
+				})
+			end, { desc = '[S]earch Recent Files ("." for repeat)' })
 
 			-- Slightly advanced example of overriding default behavior and theme
 			vim.keymap.set("n", "<leader>/", function()
@@ -531,7 +539,7 @@ require("lazy").setup({
 			local servers = {
 				-- clangd = {},
 				-- gopls = {},
-				-- pyright = {},
+				pyright = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
@@ -753,13 +761,13 @@ require("lazy").setup({
 		-- change the command in the config to whatever the name of that colorscheme is.
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"folke/tokyonight.nvim",
+		"catppuccin/nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd.colorscheme("catppuccin-macchiato")
 
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -4,14 +4,6 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
-
--- This is where you actually apply your config choices
-
 config.color_scheme = "Catppuccin Macchiato"
 -- config.default_cursor_style = 'BlinkingBlock' -- 'SteadyBar'
 config.font = wezterm.font("MesloLGS NF")
@@ -23,6 +15,29 @@ config.inactive_pane_hsb = {
 }
 
 local action = wezterm.action
+
+wezterm.on("switch-to-left", function(window, pane)
+	wezterm.log_info("left callback")
+	local tab = window:mux_window():active_tab()
+	wezterm.log_info(tab:get_pane_direction("Left"))
+	if tab:get_pane_direction("Left") ~= nil then
+		window:perform_action(wezterm.action.ActivatePaneDirection("Left"), pane)
+	else
+		window:perform_action(wezterm.action.ActivateTabRelative(-1), pane)
+	end
+end)
+
+wezterm.on("switch-to-right", function(window, pane)
+	wezterm.log_info("right callback")
+	local tab = window:mux_window():active_tab()
+	wezterm.log_info(tab:get_pane_direction("Right"))
+	if tab:get_pane_direction("Right") ~= nil then
+		window:perform_action(wezterm.action.ActivatePaneDirection("Right"), pane)
+	else
+		window:perform_action(wezterm.action.ActivateTabRelative(1), pane)
+	end
+end)
+
 config.keys = {
 	-- Mux/split panes commands
 	{
@@ -48,12 +63,14 @@ config.keys = {
 	{
 		key = "LeftArrow",
 		mods = "CMD",
-		action = action.ActivatePaneDirection("Left"),
+		-- action = action.ActivatePaneDirection("Left"),
+		action = action.EmitEvent("switch-to-left"),
 	},
 	{
 		key = "RightArrow",
 		mods = "CMD",
-		action = action.ActivatePaneDirection("Right"),
+		-- action = action.ActivatePaneDirection("Right"),
+		action = action.EmitEvent("switch-to-right"),
 	},
 	-- Natural Text Editing commands
 	{
@@ -77,7 +94,17 @@ config.keys = {
 config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
-config.tab_and_split_indices_are_zero_based = true
+config.tab_and_split_indices_are_zero_based = false
 config.show_new_tab_button_in_tab_bar = false
+
+for i = 1, 8 do
+	-- CTRL+ALT + number to move to that position
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "CTRL",
+		action = wezterm.action.MoveTab(i - 1),
+	})
+end
+
 -- and finally, return the configuration to wezterm
 return config
