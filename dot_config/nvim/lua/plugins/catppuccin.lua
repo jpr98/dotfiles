4a90bbfc48c823b4d4f691ae202d39b80fc964c2
jpr98 <jprscs@gmail.com>
2024-12-29 18:42:05 -0600

    add lualine and diffview plugins

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -6,8 +6,13 @@ return {
 	config = function()
 		vim.cmd.colorscheme("catppuccin-macchiato")
 		vim.cmd.hi("Comment gui=none")
+		require("catppuccin").setup({
+			integrations = {
+				blink_cmp = true,
+				diffview = true,
+			},
+		})
 	end,
 }
 
 -- vim: ts=4 sts=4 sw=4 et
-
--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,1 +1,21 @@
-return {}
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "catppuccin",
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
+}
+
+-- vim: ts=4 sts=4 sw=4 et
