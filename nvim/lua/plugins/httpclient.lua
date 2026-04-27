04d017bc444f82ec223d5bcdea9309b699225ff8
jpr98 <jprscs@gmail.com>
2026-04-27 14:01:22 -0400

    more settings from mba

---
+++ nvim/lua/plugins/httpclient.lua
@@ -0,0 +1,59 @@
+return {
+	"rest-nvim/rest.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			table.insert(opts.ensure_installed, "http")
+		end,
+	},
+	config = function()
+		-- The global config must run before using :Rest
+		vim.g.rest_nvim = {
+			client = {
+				preferred = "curl",
+				curl = {
+					extra_args = { "--cookie", "", "--cookie-jar", "/dev/null" },
+					-- by_host = { ["ondigitalocean%.app"] = { extra_args = { "--cookie", "", "--cookie-jar", "/dev/null" } } },
+				},
+			},
+			hooks = {
+				response = [[
+                    const keys = Object.keys(response.headers || {});
+                    for (const k of keys) {
+                        if (k.toLowerCase() === "set-cookie") {
+                            delete response.headers[k];
+                        }
+                    }
+                    if (Array.isArray(response.cookies)) {
+                        response.cookies = [];
+                    }
+                ]],
+			},
+		}
+
+		-- Format JSON responses with jq (used by rest.nvim's gq-based formatter)
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "json",
+			callback = function()
+				vim.bo.formatprg = "jq"
+			end,
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "http",
+			callback = function()
+				-- buffer-local normal-mode mapping: <leader>r -> :w<CR>:!python %<CR>
+				vim.keymap.set("n", "<leader>dr", "<cmd>Rest run<CR>", { noremap = true, silent = true, buffer = true })
+				vim.keymap.set(
+					"n",
+					"<leader>de",
+					"<cmd>Rest env select<CR>",
+					{ noremap = true, silent = true, buffer = true }
+				)
+			end,
+		})
+	end,
+}
+
+-- vim: ts=4 sts=4 sw=4 et
